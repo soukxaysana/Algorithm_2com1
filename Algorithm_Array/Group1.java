a61a1fbe53619c8e063f2d263f1d883317155a52
Kop-san <dsxd11103@gmail.com>
2023-05-09 13:39:53 +0700

    Update Group1.java

--- Algorithm_Array/Group1.java
+++ Algorithm_Array/Group1.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //Linear Search Algorithms
 
 package Algorithm_Array;
@@ -25,33 +24,3 @@ public static void main(String Args[]){
     }
     
 }
-
-=======
-//Linear Search Algorithms
-
-package Algorithm_Array;
-
-public class Group1 {
-        public static int search(int arr[], int x){
-            int N= arr.length;
-                for(int i=0 ; i < N ; i++){
-                if(arr[i] == x)
-                return i;
-            }
-            return -1;
-        }
-
-public static void main(String Args[]){
-    int arr[]= {20, 30, 40, 45, 60, 70};
-    int x= 70;
-
-    int result= search(arr, x);
-        if(result == -1)
-            System.out.println("element is not present in array");
-        else
-            System.out.println("Element is present at index " + result);
-    }
-    
-}
-
->>>>>>> Test-before-push-to-main
