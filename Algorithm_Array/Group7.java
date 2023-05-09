8ad53d54056e26f6745d89b3b1ba154f3b199325
DoXaisna <dodoxaisana123@gmail.com>
2023-05-09 13:10:59 +0700

    Update Group7.java

--- Algorithm_Array/Group7.java
+++ Algorithm_Array/Group7.java
@@ -1,30 +1,40 @@
-public class Mapping_Array {
-    public static void main(String[] args) {
-        // Define the three elements
-        String[] elements = {"Element 1", "Element 2", "Element 3"};
-        int numElements = elements.length;
+import java.util.Scanner;
 
-        // Create a 1D array for the value matrix
-        int[] valueMatrix = new int[numElements * (numElements - 1) / 2];
+public class Main {
 
-        // Assign values to represent the relationships
-        valueMatrix[getIndex(0, 1, numElements)] = 5; // Relationship between Element 1 and Element 2
-        valueMatrix[getIndex(0, 2, numElements)] = 3; // Relationship between Element 1 and Element 3
-        valueMatrix[getIndex(1, 2, numElements)] = 7; // Relationship between Element 2 and Element 3
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
 
-        // Print the value matrix
-        int index = 0;
-        for (int i = 0; i < numElements; i++) {
-            for (int j = i + 1; j < numElements; j++) {
-                System.out.println("Relationship between " + elements[i] + " and " + elements[j] + ": " +
-                        valueMatrix[index]);
-                index++;
+        for (int i = 0; i < n - 1; i++) {
+            int minIdx = i;
+
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIdx]) {
+                    minIdx = j;
+                }
             }
+
+            int temp = arr[minIdx];
+            arr[minIdx] = arr[i];
+            arr[i] = temp;
         }
     }
 
-    // Calculate the flattened index based on row and column indices
-    private static int getIndex(int row, int col, int numElements) {
-        return col + (row * numElements) - (row * (row + 1) / 2) - row - 1;
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int[] arr = new int[3];
+
+        for (int i = 0; i < 3; i++) {
+            System.out.print("Enter element " + (i+1) + ": ");
+            arr[i] = input.nextInt();
+        }
+
+        selectionSort(arr);
+
+        System.out.println("\nSorting Number of Array:");
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " " );
+        }
     }
 }
